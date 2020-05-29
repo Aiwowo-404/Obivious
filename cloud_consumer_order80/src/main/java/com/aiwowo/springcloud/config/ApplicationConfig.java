1013dc3bfcc50df06754fbe8d490983194650d85
Aiwowo-404 <924848076@qq.com>
2020-05-29 10:41:56 +0800

    consumer和provider微服务注册进eureka

---
+++ cloud_consumer_order80/src/main/java/com/aiwowo/springcloud/config/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.aiwowo.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public RestTemplate RestTemplate(){
+        return new RestTemplate();
+    }
+}
---
+++ cloud_consumer_order80/src/main/java/com/aiwowo/springcloud/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.aiwowo.springcloud.controller;
+
+import com.aiwowo.springcloud.entity.CommonResult;
+import com.aiwowo.springcloud.entity.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class OrderController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public static final String URL = "http://localhost:8001";
+
+    @GetMapping("/consumer/get/{id}")
+    @ResponseBody
+    public CommonResult<Payment> getPayment(@PathVariable("id")int id){
+        CommonResult commonResult=restTemplate.getForObject(URL+"/payment/get/"+id,CommonResult.class);
+        return commonResult;
+    }
+
+    @PostMapping("/consumer/create")
+    @ResponseBody
+    public CommonResult createPayment(@RequestBody Payment payment){
+        CommonResult commonResult = restTemplate.postForObject(URL+"/payment/create",payment,CommonResult.class);
+        return commonResult;
+    }
+}
